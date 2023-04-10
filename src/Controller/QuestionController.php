c1eaa8a04d0161f3ea7ba2c47818d059540df76d
YBogdan RADOI <radoi.office@gmail.com>
2023-04-10 10:23:05 +0200

    Récupère les paramètres POST request

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,6 +8,7 @@ use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -92,5 +93,19 @@ class QuestionController extends AbstractController
             'question' => $question,
             'answers' => $answers,
         ]);
+    }
+
+    /**
+     * @Route("/questions/{slug}/vote", name="app_question_vote", methods={"POST"})
+     */
+    public function questionVote(Request $request, Question $question, EntityManagerInterface $entityManager)
+    {
+        $direction = $request->request->get('direction');
+        if ($direction === 'up') {
+            $question->setVotes($question->getVotes()+1);
+        } elseif($direction === 'down') {
+            $question->setVotes($question->getVotes()+1);
+        }
+        return ;
     }
 }
